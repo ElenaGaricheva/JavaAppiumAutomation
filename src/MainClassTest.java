bd8fe798ddde5add48fe90844a15c0d12ace01d6
Elena Garicheva <elenagaricheva95@gmail.com>
2020-03-22 20:30:36 +0300

    test_1: verification of the returned number

---
+++ src/MainClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+
+    MainClass mainClass = new MainClass();
+
+    @Test
+    public void testGetLocalNumber() {
+        Assert.assertEquals("Function returned incorrect value", 14, mainClass.getLocalNumber());
+    }
+}
