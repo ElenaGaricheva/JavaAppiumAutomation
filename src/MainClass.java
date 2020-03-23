6d4cb206628581c1aa34c1959210f21c8b719591
Elena Garicheva <elenagaricheva95@gmail.com>
2020-03-23 11:43:28 +0300

    test_2: verification of the returned number is bigger than 45

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,15 @@
 public class MainClass {
 
+    private int class_number = 20;
+
+    public int getClassNumber() {
+
+        return this.class_number;
+
+    }
+
     public int getLocalNumber() {
+
         return 14;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,9 @@ public class MainClassTest {
     public void testGetLocalNumber() {
         Assert.assertEquals("Function returned incorrect value", 14, mainClass.getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertFalse("The returned value is bigger or equal 45", mainClass.getClassNumber() > 45);
+    }
 }
